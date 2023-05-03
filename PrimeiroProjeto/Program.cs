4ee9e81d60459ae6daf117afe5bf0e1a3bdd1db0
Luiz Felipe <lffm1994@gmail.com>
2023-05-03 00:01:41 -0300

    Primeiro programa C#

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeiroProjeto 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Nova Versão
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
